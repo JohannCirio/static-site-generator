51d710a5308c350f9f04dc279345d24b599c9b8a
Johann <johannocsantos@gmail.com>
2024-06-26 10:19:52 -0300

    Adds tests for the img and link extractors

--- src/main.py
+++ src/main.py
@@ -2,15 +2,15 @@ from textnode import TextNode
 from htmlnode import HTMLNode
 from htmlnode import LeafNode
 from inline_markdown import split_nodes_delimiter
-from images_markdown import extract_markdown_images
-from images_markdown import extract_markdown_links
+from inline_markdown import extract_markdown_images
+from inline_markdown import extract_markdown_links
 
 def main():
     text_with_img = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
     text_without_img = 'This is text'
     text_with_img = "This is text with an [image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
 
-    extract_markdown_images(text_with_img)
+    print(extract_markdown_images(text_with_img))
     print(extract_markdown_images(text_without_img))
     print(extract_markdown_links(text_with_img))
 
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 
 from inline_markdown import split_nodes_delimiter
+from inline_markdown import extract_markdown_images
+from inline_markdown import extract_markdown_links
 from textnode import TextNode
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -171,3 +173,67 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             result = split_nodes_delimiter(text_list, "**", 'bold')
 
         self.assertEqual(str(context.exception), "Closing Limiter not found, invalid Markdown Syntax")
+
+class TestImageExtractor(unittest.TestCase):
+    def test_one_img_present(self):
+        text = "This is text with an ![alt text](http://blabla.com/img)"
+        desired_result = [("alt text", "http://blabla.com/img")]
+    
+        self.assertEqual(extract_markdown_images(text), desired_result)
+    
+    def test_two_imgs_present(self):
+        text = "This is text with an ![alt text](http://blabla.com/img) and another one ![text alt](http://gremio.com/img)"
+        desired_result = [("alt text", "http://blabla.com/img"), ("text alt", "http://gremio.com/img")]
+    
+        self.assertEqual(extract_markdown_images(text), desired_result)
+
+    def test_no_img(self):
+        text = "This is text with no img"
+        desired_result = []
+    
+        self.assertEqual(extract_markdown_images(text), desired_result)
+
+    def test_with_link(self):
+        text = "This is text with link [link](http://blabla.com/img)"
+        desired_result = []
+    
+        self.assertEqual(extract_markdown_images(text), desired_result)
+
+    def test_with_link_and_img(self):
+        text = "This is text with link [link](http://blabla.com/img) and img ![alt text](http://blabla.com/img)"
+        desired_result = [("alt text", "http://blabla.com/img")]
+    
+        self.assertEqual(extract_markdown_images(text), desired_result)
+
+
+    class TestLinkExtractor(unittest.TestCase):
+        def test_one_link_present(self):
+            text = "This is text with an [link](http://blabla.com/img)"
+            desired_result = [("link", "http://blabla.com/img")]
+        
+            self.assertEqual(extract_markdown_links(text), desired_result)
+        
+        def test_two_links_present(self):
+            text = "This is text with an [link 1](http://blabla.com/img) and another one [link 2](http://gremio.com/img)"
+            desired_result = [("link 1", "http://blabla.com/img"), ("link 2", "http://gremio.com/img")]
+        
+            self.assertEqual(extract_markdown_links(text), desired_result)
+
+        def test_no_link(self):
+            text = "This is text with no link"
+            desired_result = []
+        
+            self.assertEqual(extract_markdown_links(text), desired_result)
+
+        def test_with_img(self):
+            text = "This is text with link ![alt text](http://blabla.com/img)"
+            desired_result = []
+        
+            self.assertEqual(extract_markdown_links(text), desired_result)
+
+        def test_with_link_and_img(self):
+            text = "This is text with link [link](http://blabla.com/img) and img ![alt text](http://blabla.com/img)"
+            desired_result = [("link", "http://blabla.com/link")]
+        
+            self.assertEqual(extract_markdown_links(text), desired_result)
+
